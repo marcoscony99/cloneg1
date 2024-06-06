54273f8619c5a1f4ce3a93164a049f49d85c10f6
Marcos Cony <marcoscony@gmail.com>
2024-06-06 12:48:07 -0300

    define_home

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 
 app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return redirect('/cloneg1')
 
 @app.route('/cloneg1')
 def cloneg1():
